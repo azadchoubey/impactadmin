d1a1f11c9f53cd38def1184bb1da9ae2e481a956
Azad Choubey <mr_azadchoubey@outlook.com>
2024-01-08 15:26:27 +0530

    Login

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+<livewire:login /> 
+@endsection
---
+++ resources/views/livewire/login.blade.php
@@ -0,0 +1,7 @@
+
+<div>
+    {{-- Care about people's approval and you will be their prisoner. --}}
+    <h3>Login</h3>
+</div>
+
+
