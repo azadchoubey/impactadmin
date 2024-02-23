847ff379aa98ab2193753401fa69b7ee60a3f550
anshsinglaaa <148761020+anshsinglaaa@users.noreply.github.com>
2024-02-23 14:13:54 +0530

    update by ansh

--- app/Http/Controllers/ClientsProfile.php
+++ app/Http/Controllers/ClientsProfile.php
@@ -11,14 +11,10 @@ class ClientsProfile extends Controller
 {
     public function index($id)
     {
-        $data = Clinetprofile::with('contacts', 'contacts.delivery', 'contacts.regularDigestPrint', 'contacts.regularDigestWeb', 'Country', 'region', 'sector', 'keywords')->find($id);;
+        $data = Clinetprofile::with('contacts', 'contacts.delivery', 'contacts.regularDigestPrint', 'contacts.regularDigestWeb', 'Country', 'region', 'sector', 'keywords','billingcycle')->find($id);;
         $keywords = $data->keywords;
         $contacts = $data->contacts;
         $editing = '';
-<<<<<<< Updated upstream
         return view('clients', compact('data', 'contacts', 'keywords', 'editing'));
-=======
-       return view('clients',compact('contacts','keywords','editing','data'));
->>>>>>> Stashed changes
     }
 }
