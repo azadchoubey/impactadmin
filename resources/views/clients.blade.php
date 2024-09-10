d42f13003d5d24e1bcd7189eb6f3ef99d0124b04
anshsinglaaa <148761020+anshsinglaaa@users.noreply.github.com>
2024-09-10 15:23:04 +0530

    Update

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -500,10 +500,10 @@ $keywordcategories = \App\Models\Picklist::where('type','keyword category')->ord
                         <x-print-concept :getprintissueforclients="$getprintissueforclients" :clientid="$data->ClientID"/>
                     </div>
                     <div id="print-complex-concept" role="tabpanel" aria-labelledby="print-complex-concept-tab">
-                        Print content for Complex Concept
+                        {{-- <x-print-complex-concept :getprintissueforclients="$getprintissueforclients" :clientid="$data->ClientID"/> --}}
                     </div>
                     <div id="print-issues" role="tabpanel" aria-labelledby="print-issues-tab">
-                        Print content for Issues
+                        {{-- <x-print-issue-defination :getprintissueforclients="$getprintissueforclients" :clientid="$data->ClientID"/> --}}
                     </div>
                 </div>
             </div>
